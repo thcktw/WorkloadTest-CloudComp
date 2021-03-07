2686920a95ee2d212e221ee0d37c66c9a65e07c7
armykongtap <army_kongtap@hotmail.com>
2021-03-07 21:39:15 +0700

    update setting

--- backend/settings.py
+++ backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "5!p_2ho(w3o-o112l(15mw&z#gws7rwul#4&3mp47bfsu!j-ah"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
