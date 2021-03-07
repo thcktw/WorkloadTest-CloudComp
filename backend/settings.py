bcb1278ce1888ff0489e2bfb91debec7c852c46c
armykongtap <army_kongtap@hotmail.com>
2021-03-07 22:06:35 +0700

    update setting

--- backend/settings.py
+++ backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "5!p_2ho(w3o-o112l(15mw&z#gws7rwul#4&3mp47bfsu!j-ah"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
